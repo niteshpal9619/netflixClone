50ac58c116f91f6d4c2ce5f0a059dad140ffe56d
niteshpal9619 <mssnv42@gmail.com>
2023-12-06 18:00:09 +0530

    new Website1.html Project is added

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    build: {
+      outDir: 'dist', // specify your output directory
+      assetsDir: 'assets', // specify your assets directory
+    },
+    base: '/', // specify your base path if needed
+  };
